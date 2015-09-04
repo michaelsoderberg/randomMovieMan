a72c760326c70764a3dcc586ed82421952721dbf
Pär Strandberg <Par.Strandberg@comhem.com>
2015-09-05 00:22:23 +0200

    Cleanup js. Getting a movie

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-$('.sendRequest').click(function(){
-      sendRequest();      
+    sendRequest();
 
-      function sendRequest() {
-      		var parms = "title=" + $("#textBox").val() + "&format=JSONP";
-      		
-      		// Other parameters
-      		//parms += "&lang=en-us&actors=S";
-      		
-      		$("#countries").text("");
-      		$("#actors").text("");
-      		
-      		$.ajax({
-                  data:		parms,
-                  url:		'http://www.myapifilms.com/imdb',
-                  type:		'get',
-                  dataType:	'jsonp',
-                  beforeSend: function () {},
-                  success:  function (response, textStatus, jqXHR) {
-                  	$.each(response, function(index, element){
-                  		if (element.countries != undefined) {
-                  			$.each(element.countries, function(index, country){
-                  				$("#countries").append(country + ", ");
-                  			});
-                  		}
-                  		if (element.actors != undefined) {
-                  			$.each(element.actors, function(index, actor){
-                  				$("#actors").append(actor.actorName + " - " + actor.actorId + ", ");
-                  			});
-                  		}
-                  	});
-                  },
-                  error: function(jqXHR, textStatus, errorThrown) {
-                  	$("#error").text(textStatus + "; " + errorThrown);
-                  }
-          	});
-      	}
-      });
+    function sendRequest() {
+        //var parms = "title=" + $("#textBox").val() + "&format=JSONP";
+        var parms = "?title=air%bud&format=JSONP";
+
+        $("#title").text("");
+        $("#genres").text("");
+        $("#actors").text("");
+        $("#rating").text("");
+        $("#url").text("");
+
+        $.ajax({
+            data: parms,
+            url: 'http://www.myapifilms.com/imdb',
+            type: 'GET',
+            data: {
+                title: "mvp",
+                lang: "en-us",
+                filter: "M",
+                format: "JSONP"
+            },
+            dataType: 'jsonp',
+            success: function (response, textStatus, jqXHR) {
+                console.log('Haaaam guuuurl!');
+                printMovie(response[0]);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#error").text(textStatus + "; " + errorThrown);
+            }
+        });
+    }
+
+    function printMovie(movie) {
+        console.log(movie);
+
+        $("#movie-title").attr("href", movie.urlIMDB).text(movie.title);
+        $("#rating").append(movie.rating + ", ");
+
+        $.each(movie.genres, function (i, genre) {
+            $("#genres").append(genre + ', ');
+        });
+
+        $.each(movie.actors, function (i, actor) {
+            $("#actors").append(actor.actorName + " - " + actor.actorId + ", ");
+        });
+    }
 });
+
+<!-- End document ready -->
